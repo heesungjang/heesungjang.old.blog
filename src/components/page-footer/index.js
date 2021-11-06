91f57c203d10203b478edac3f83f3f73ea7e3f06
Heesung jang <68782077+heesungjang@users.noreply.github.com>
2021-11-07 04:42:48 +0900

    .

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -4,13 +4,7 @@ import './style.scss';
 
 const PageFooter = ({ author, githubUrl }) => (
   <>
-    <footer className="page-footer-wrapper">
-      <p className="page-footer">
-        © {new Date().getFullYear()}
-        &nbsp;
-        <a href={githubUrl}>{author}</a>
-      </p>
-    </footer>
+    <footer className="page-footer-wrapper"></footer>
   </>
 );
 
